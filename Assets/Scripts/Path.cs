ba9fb8379014559a10dea4a43e29e67a47760566
Jochem <Jochem@DESKTOP-213E14N>
2017-10-09 20:49:26 +0200

    Pushing everything

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public Color lineColor;
+    private List<Transform> nodes = new List<Transform>();
+
+    //TO make the path visible in the editor
+    public void OnDrawGizmos()
+    {
+        //Set color for editor
+        Gizmos.color = lineColor;
+        
+        //Get all nodes
+        var pathTransforms = GetComponentsInChildren<Transform>();
+        
+        //All nodes that are not the top node
+
+        nodes = pathTransforms.Where(x => x != transform).ToList();
+
+
+        //Draw line between every node
+        for (var i = 0; i < nodes.Count; i++)
+        {
+            //Check if node is not going to high
+            if (i + 1 > nodes.Count) continue;
+
+            var currentNode = nodes[i].position;
+            var nextNode = nodes[i + 1].position;
+            Gizmos.DrawLine(currentNode, nextNode);
+        }
+    }
+}
